4e037b0fa6d16c5c7e758a7c4681954791013615
Bilal Bhurgri <billyballerina04@gmail.com>
2025-01-26 16:29:56 -0800

    Added authentication functions

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,16 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import {APP, ANALYTICS, AUTH} from "./firebase-config";
+
 const express = require('express');
 const cors = require('cors');
 
 const port = 3000;
 const app = express();
 app.use(cors());
+
+app.post('/api/auth', auth);
+app.get('/api/login', login);
+
 
 /** GET endpoint for sending back a Hello World message */
 app.get('/hello', (req, res) => {
@@ -14,4 +21,18 @@ app.get('/hello', (req, res) => {
 // Tells our app to listen to the given port
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.post('/api/auth', (req, res) => {
+  const auth = AUTH;
+  createUserWithEmailAndPassword(auth, req.email, req.password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      // would put in a function to retrieve users data on success
+      res.status(200).send("Successful account creation");
+    })
+    .catch((error) => {
+      res.status(400).send("Invalid email provided");
+    });
+
 });
