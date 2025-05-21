e8ad621d1da34caf5077bf9da4519e0da4c40298
Wesley Li <orangewesli@gmail.com>
2025-05-20 17:57:31 -0700

    Fixed Mapbox on ios

--- app/HomeScreen.tsx
+++ app/HomeScreen.tsx
@@ -1,24 +1,26 @@
 import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
-import Mapbox, {MapView} from "@rnmapbox/maps";
+import Mapbox, {MapView, Camera} from "@rnmapbox/maps";
 
 
-Mapbox.setAccessToken("pk.eyJ1Ijoic2Vhbmx1dSIsImEiOiJjbTd2MjB5bWowNzV5MmpuNGptbTNocjdnIn0.dNhHkY6L8_EC1VAmK-obaw");
+Mapbox.setAccessToken("sk.eyJ1Ijoic2Vhbmx1dSIsImEiOiJjbWF4MHE1a2kwbDN2Mm5vaXhreHE3Y2R2In0.Z1IhV2SDnwGdU1FR0v4qwg");
 
 const styles = StyleSheet.create({
     page: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#F5FCFF"
+        backgroundColor: "#F5FCFF",
     },
     container: {
-        height: 300,
-        width: 300,
+        flex: 1,
+        width: "100%",
+        height: '100%',
         backgroundColor: "tomato"
     },
     map: {
-        flex: 1
+        flex: 1,
+        paddingTop: -100
     }
 })
  
@@ -31,7 +33,15 @@ export default class App extends Component {
       return (
         <View style={styles.page}>
           <View style={styles.container}>
-            <MapView style={styles.map} />
+            <MapView style={styles.map} styleURL={Mapbox.StyleURL.SatelliteStreet}> 
+                <Camera
+                    zoomLevel={13}
+                    centerCoordinate={[-122.3035, 47.6556]} // longitude, latitude (UW)
+                    pitch={0}
+                    heading={0}
+                    animationMode={'flyTo'}
+                    animationDuration={1000}/>
+            </MapView>
           </View>
         </View>
       );
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,5 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginScreen from "./LoginScreen";
-import HomeScreen from './HomeScreen';
-
-const Stack = createNativeStackNavigator();
 
 
 const App = () => {
